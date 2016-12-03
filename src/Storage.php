a6a7c0eccfc4deb2649ca4f12ddb8a536fb7a9ef
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-03 10:11:53 +0100

    init

---
+++ src/Storage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Translation\Common;
+
+/**
+ * The storage is a place when you can store your translations. A database, filesystem or a third party platform
+ */
+interface Storage
+{
+
+}
