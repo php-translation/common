b12066a594b1668e85cf9f869a68cc8125406887
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-05 09:43:43 +0100

    Added exception (#5)
    * Added exception
    
    * cs
    
    * cs

---
+++ src/Exception/StorageException.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the PHP Translation package.
+ *
+ * (c) PHP Translation team <tobias.nyholm@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Translation\Common\Exception;
+
+use Translation\Common\Exception;
+
+/**
+ * Storage related exceptions.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class StorageException extends \RuntimeException implements Exception
+{
+    public static function translationExists($key, $domain)
+    {
+        return new self(sprintf('You cannot create a new translation with key "%s". That key does already exist in domain "%s".', $key, $domain));
+    }
+}
