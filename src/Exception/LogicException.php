ad30e44f0f767ecb47d20601a750c07c24b23a82
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-09 20:27:20 +0100

    Added exception

---
+++ src/Exception/LogicException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Translation\Common\Exception;
+
+use Translation\Common\Exception;
+
+/**
+ *
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class LogicException extends \LogicException implements Exception
+{
+
+}
