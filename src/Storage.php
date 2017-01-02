f675dc2c4291c80a645a5a52e214fa6684fa87b8
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-01-02 14:01:03 +0100

    Patch 2 (#15)
    * updated docs
    
    * Added docs
    
    * Typo

--- src/Storage.php
+++ src/Storage.php
@@ -22,7 +22,7 @@ use Translation\Common\Model\Message;
 interface Storage
 {
     /**
-     * Get a translation.
+     * Get a translation. If no translation is found, null MUST be returned.
      *
      * @param string $locale
      * @param string $domain
